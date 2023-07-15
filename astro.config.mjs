8e29660b62826afb88521192a03999126e8283c3
user12043 <user12043@hotmail.com>
2023-07-15 17:13:53 +0300

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-    site: "https://user12043.github.com"
+  site: "https://user12043.github.com",
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes("user12043.github.io/resume"),
+      changefreq: "weekly"
+    })
+  ]
 });
